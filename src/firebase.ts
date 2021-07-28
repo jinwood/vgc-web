1d2baecffdd6e6ba32ec4cf901f8a44af984d3a3
jinwood <jinw@protonmail.com>
2021-07-28 21:22:12 +0100

    Init

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+import * as firebase from "firebase";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB89hbXjN5va48ILEcez9IZeXRgtbzeOjs",
+  authDomain: "vgc-app.firebaseapp.com",
+  projectId: "vgc-app",
+  storageBucket: "vgc-app.appspot.com",
+  messagingSenderId: "33297933371",
+  appId: "1:33297933371:web:dfd3fe1bab5c0aec2fa81e",
+  measurementId: "G-JTR1FQ600Q",
+};
